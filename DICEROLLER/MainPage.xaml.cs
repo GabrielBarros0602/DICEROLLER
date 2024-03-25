b5fd0afbc6f2356bdf5ff71a1aa02e75ea0e4417
Gabriel Barros Cisoto <gabrielbarros.ctt.profissional@gmail.com>
2024-03-25 20:41:55 -0300

    Atividade Avaliativa

---
+++ DICEROLLER/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DICEROLLER
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollClicked(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            ResultadoRolagem.Text = ($"{rnd.Next(1, 10)}");
+                
+        }
+
+
+    }
+
+}
